23216ac644093d327f66a61919fbb1f7658f48ef
fgutta <francisgutta@gmail.com>
2026-09-07 13:26:45 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://avantisfs.ca",
-	output: 'server',
+	output: 'static',
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
