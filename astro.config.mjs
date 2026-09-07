57099f543ff6eeb337fd83208ea56c4bb399ca1c
fgutta <francisgutta@gmail.com>
2026-09-07 11:29:56 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import cloudflare from '@astrojs/cloudflare';
-//import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://avantisfs.ca",
 	output: 'server',
-	integrations: [mdx()],
+	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
